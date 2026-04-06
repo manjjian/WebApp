a05d43d827aa70e0e39e86f6edcbbf16f53d9485
manjjian <138691186+manjjian@users.noreply.github.com>
2026-04-06 18:45:08 +0900

    Create Header.jsx

---
+++ 0401/Header.jsx
@@ -0,0 +1,15 @@
+// src/components/Header.jsx
+import React from 'react';
+
+const Header = () => (
+  <nav className="navbar">
+    <div className="logo">MINJIN.LOG</div>
+    <ul>
+      <li>About</li>
+      <li>Projects</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+export default Header;
